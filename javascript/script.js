94f8c011f2168dc42d223255f769d954d2dc0395
Jhon Florez <134735225+Eddyzzero@users.noreply.github.com>
2024-07-03 14:44:55 +0200

    creation of the main pages and css, some animations and scrolls

---
+++ javascript/script.js
@@ -0,0 +1,58 @@
+
+//ajouter scroll à l'image
+window.addEventListener('scroll', function() {
+    const scrollImage = document.querySelector('.nit-guepes');
+    const scrollTop = window.scrollY || window.pageYOffset;
+
+    // Modifier la position verticale de l'image en fonction du défilement
+    scrollImage.style.top = `-${scrollTop}px`;
+});
+
+// ajouter hidden à mon texte
+window.addEventListener('scroll',reveal);
+
+function reveal (){
+    const reveals = document.querySelectorAll('.parallax-gregory-text');
+
+    for(let i = 0; i < reveals.length; i++) {
+        let hauteurFenetre = window.innerHeight;
+        let VoirHautFenetre = reveals[i].getBoundingClientRect().top;
+        let pointVoir = 150;
+
+        //creer l'animation scroll
+        if ( VoirHautFenetre < hauteurFenetre -pointVoir){
+            reveals[i].classList.add('active');
+        }
+        else{
+            reveals[i].classList.remove('active');
+        }
+    }
+}
+
+let faireDefiler = 300;
+let quantiteDefilement = 0;
+
+window.addEventListener('scroll', () =>{
+    quantiteDefilement = window.scrollY;
+    let containerInfosGre = document.querySelector('.container-infos-gre');
+    if ( quantiteDefilement > faireDefiler ) {
+        containerInfosGre.classList.add('defilement');
+    } else {
+        containerInfosGre.classList.remove('defilement');
+    }
+});
+
+let textCache = document.querySelector('.text-extermination-guepes');
+let faireDefilerHidden = 2;
+
+window.addEventListener('scroll', () => {
+    if ( window.scrollY > faireDefilerHidden || window.scrollY < -faireDefilerHidden ) {
+    textCache.classList.remove('defilement-hidden');
+    } else {
+        textCache.classList.add('defilement-hidden');
+    }
+});
+
+
+
+ 
